4a163b5fb8815f5e03d9b84681ca301c7ee77da3
dzakyzf <dzakybd@gmail.com>
2017-12-04 20:51:26 +0700

    Add addfillingactivity + layout + label in manifest

---
+++ app/src/main/java/com/kpl/sandwichshop/activities/AddFillingActivity.java
@@ -0,0 +1,15 @@
+package com.kpl.sandwichshop.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kpl.sandwichshop.R;
+
+public class AddFillingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_filling);
+    }
+}
