110b81411df165fb4dbbe772067d2b4bd9eb02ea
M Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2017-12-05 21:21:26 +0700

    minor change, remove unused

--- app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
+++ app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
@@ -2,8 +2,6 @@ package com.kpl.sandwichshop.activities;
 
 import android.app.NotificationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -26,25 +24,14 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void onOrderClicked(View view) {
-    // Subscribe to notifications
         sandwich.register(order);
         sendNotification(order.update());
     }
 
     public void onUpdateClicked(View view) {
-    // Mimic message from server
         sandwich.setReady(true);
         sendNotification(order.update());
     }
@@ -54,12 +41,12 @@ public class OrderActivity extends AppCompatActivity {
                 (NotificationCompat.Builder)
                         new NotificationCompat.Builder(this)
                                 .setSmallIcon(R.drawable.ic_launcher_round)
-                                .setContentTitle("Sandwich Factory")
+                                .setContentTitle("Sandwich Shop")
                                 .setContentText(message);
         NotificationManager manager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
         manager.notify(notificationId, builder.build());
-    // Update notifications if needed
+
         notificationId += 1;
     }
 
