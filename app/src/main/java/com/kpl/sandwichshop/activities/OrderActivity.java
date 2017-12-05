66378f91e3dab4b539f58ea90cea338e00f6150f
jeffrynasri <jeffryfaruki@Gmail.com>
2017-12-05 21:35:17 +0700

    Tambah Tombol back order activity

--- app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
+++ app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.kpl.sandwichshop.R;
@@ -24,6 +25,7 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void onOrderClicked(View view) {
@@ -49,5 +51,20 @@ public class OrderActivity extends AppCompatActivity {
 
         notificationId += 1;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
+    }
 }
