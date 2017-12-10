5cf53052bf4147cdcd8280548afc269a3812e5d6
M Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2017-12-10 23:09:57 +0700

    add cash, credit card for payment methode. Add status payment activity

--- app/src/main/java/com/kpl/sandwichshop/activities/PaymentActivity.java
+++ app/src/main/java/com/kpl/sandwichshop/activities/PaymentActivity.java
@@ -1,8 +1,13 @@
 package com.kpl.sandwichshop.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.kpl.sandwichshop.R;
 
@@ -10,13 +15,24 @@ import com.kpl.sandwichshop.R;
  * Created by Ilham Aulia Majid on 01-Dec-17.
  */
 
-public class PaymentActivity  extends AppCompatActivity {
+public class PaymentActivity  extends AppCompatActivity implements View.OnClickListener{
+    LinearLayout creditLayout, cash, credit_card;
+    Button bayar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        creditLayout = (LinearLayout) findViewById(R.id.card_payment);
+        credit_card = (LinearLayout) findViewById(R.id.credit_card);
+        credit_card.setOnClickListener(this);
+        cash = (LinearLayout) findViewById(R.id.cash);
+        cash.setOnClickListener(this);
+        bayar = findViewById(R.id.btn_bayar);
+        bayar.setOnClickListener(this);
+
     }
     @Override
     public void onBackPressed() {
@@ -34,6 +50,18 @@ public class PaymentActivity  extends AppCompatActivity {
         }
     }
 
-
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_bayar:
+                startActivity(new Intent(this, StatusActivity.class));
+                break;
+            case R.id.credit_card:
+                creditLayout.setVisibility(View.VISIBLE);
+                break;
+            case R.id.cash:
+                startActivity(new Intent(this, StatusActivity.class));
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/kpl/sandwichshop/activities/StatusActivity.java
@@ -0,0 +1,15 @@
+package com.kpl.sandwichshop.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kpl.sandwichshop.R;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
