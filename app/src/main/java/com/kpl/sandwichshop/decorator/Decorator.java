b66ce37b29636b21e958d840c6ef3a584d0ebb1f
dzaky <dzakybd@gmail.com>
2017-12-12 23:00:25 +0700

    fix decorator super class

--- app/src/main/java/com/kpl/sandwichshop/decorator/Decorator.java
+++ app/src/main/java/com/kpl/sandwichshop/decorator/Decorator.java
@@ -1,18 +1,42 @@
 package com.kpl.sandwichshop.decorator;
 
+import com.google.auto.value.AutoValue;
+
+import org.parceler.Parcel;
+import org.parceler.ParcelFactory;
+import org.parceler.ParcelProperty;
+
 /**
  * Created by Ilham Aulia Majid on 05-Dec-17.
  */
 
+@AutoValue
+@Parcel
 public abstract class Decorator {
 
-    private String name;
-    private int price;
-    private Decorator decorator;
+    @ParcelProperty("name")
+    public String name;
+    @ParcelProperty("price")
+    public int price;
+    @ParcelProperty("Decorator")
+    public Decorator decorator;
 
-    public Decorator(Decorator decozrator) {
-        this.decorator = decorator;
+    @ParcelFactory
+    public static Decorator create(String name) {
+        if (name.contentEquals("Mayonnaise")) {
+            return new MayoDecorator();
+        }else if (name.contentEquals("Cheese Sauce")) {
+            return new CheeseDecorator();
+        }else if (name.contentEquals("BBQ Sauce")) {
+            return new SauceDecorator();
+        }else{
+            return null;
+        }
     }
+
+//    public Decorator(Decorator decorator) {
+//
+//    }
 
     public void setName(String name) {
         this.name = name;
