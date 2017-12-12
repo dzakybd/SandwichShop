e1c8500d8d3dece0086d970c319fefbe7e494358
M Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2017-12-12 21:10:06 +0700

    implement observer on NotificationObserver and HistoryObserver

--- app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
+++ app/src/main/java/com/kpl/sandwichshop/activities/OrderActivity.java
@@ -1,71 +1,60 @@
 package com.kpl.sandwichshop.activities;
 
-import android.app.NotificationManager;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.kpl.sandwichshop.R;
-import com.kpl.sandwichshop.builder.Sandwich;
-import com.kpl.sandwichshop.observer.Observer;
-import com.kpl.sandwichshop.observer.Order;
 
 public class OrderActivity extends AppCompatActivity {
 
-    Sandwich sandwich = new Sandwich();
-    Observer order = new Order(sandwich);
-    int notificationId = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void onOrderClicked(View view) {
-        sandwich.register(order);
-        sendNotification(order.update());
-    }
-
-    public void onUpdateClicked(View view) {
-        sandwich.setReady(true);
-        sendNotification(order.update());
-    }
-
-    private void sendNotification(String message) {
-        NotificationCompat.Builder builder =
-                (NotificationCompat.Builder)
-                        new NotificationCompat.Builder(this)
-                                .setSmallIcon(R.drawable.ic_notification)
-                                .setContentTitle("Sandwich Shop")
-                                .setContentText(message);
-        NotificationManager manager = (NotificationManager)
-                getSystemService(NOTIFICATION_SERVICE);
-        manager.notify(notificationId, builder.build());
-
-        notificationId += 1;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        finish();
-        super.onBackPressed();
-    }
+//    Sandwich sandwich = new Sandwich();
+//    Observer order = new NotificationObserver(sandwich);
+//    int notificationId = 1;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_order);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//    }
+//
+//    public void onOrderClicked(View view) {
+//        sandwich.register(order);
+//        sendNotification(order.update());
+//    }
+//
+//    public void onUpdateClicked(View view) {
+//        sandwich.setReady(true);
+//        sendNotification(order.update());
+//    }
+//
+//    private void sendNotification(String message) {
+//        NotificationCompat.Builder builder =
+//                (NotificationCompat.Builder)
+//                        new NotificationCompat.Builder(this)
+//                                .setSmallIcon(R.drawable.ic_notification)
+//                                .setContentTitle("Sandwich Shop")
+//                                .setContentText(message);
+//        NotificationManager manager = (NotificationManager)
+//                getSystemService(NOTIFICATION_SERVICE);
+//        manager.notify(notificationId, builder.build());
+//
+//        notificationId += 1;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                onBackPressed();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        finish();
+//        super.onBackPressed();
+//    }
 }
---
+++ app/src/main/java/com/kpl/sandwichshop/observer/HistoryObserver.java
@@ -0,0 +1,20 @@
+package com.kpl.sandwichshop.observer;
+
+import com.kpl.sandwichshop.Order;
+import com.kpl.sandwichshop.builder.Sandwich;
+
+/**
+ * Created by Hendry on 12/12/2017.
+ */
+
+public class HistoryObserver implements Observer{
+
+    public HistoryObserver() {
+
+    }
+
+    @Override
+    public void update(Order order) {
+
+    }
+}
