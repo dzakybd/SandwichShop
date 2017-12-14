cee4ba62dbddb10e4309bb66e34731ffdb709a88
M Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2017-12-14 12:54:00 +0700

    add status info order using alert dialog

--- app/src/main/java/com/kpl/sandwichshop/activities/StatusActivity.java
+++ app/src/main/java/com/kpl/sandwichshop/activities/StatusActivity.java
@@ -1,7 +1,11 @@
 package com.kpl.sandwichshop.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.kpl.sandwichshop.Order;
@@ -18,9 +22,8 @@ import org.parceler.Parcels;
 
 public class StatusActivity extends AppCompatActivity {
 
-    TextView textViewOrderInfo;
-    Sandwich sandwich = new Sandwich();
-    int notificationId = 1;
+    TextView textViewOrderInfo, textViewStatusInfo;
+    Button buttonOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,26 @@ public class StatusActivity extends AppCompatActivity {
         Observer historyObserver = new HistoryObserver();
         final Observable orderObservable = new OrderObservable(order);
 
-        textViewOrderInfo = findViewById(R.id.textview_order_info);
-        textViewOrderInfo.setText(order.toString());
+//        textViewOrderInfo = findViewById(R.id.textview_order_info);
+//        textViewOrderInfo.setText(order.toString());
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(StatusActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_status, null);
+        textViewStatusInfo = (TextView) mView.findViewById(R.id.textview_status_info);
+        buttonOk = (Button) mView.findViewById(R.id.button_ok);
+        textViewStatusInfo.setText(order.toString());
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
 
         orderObservable.register(notificationObserver);
         orderObservable.register(historyObserver);
@@ -41,7 +62,7 @@ public class StatusActivity extends AppCompatActivity {
         Thread observerTherad = new Thread(){
             public void run(){
                 try{
-                    sleep(5000);
+                    sleep(4000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
